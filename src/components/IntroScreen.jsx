4d8e7086d6d70756f157237ee249b4f1445b11fc
Mikkelbilgrav1 <eaa23msb@students.eaaa.dk>
2025-05-22 17:13:21 +0200

    aesdad

--- src/components/IntroScreen.jsx
+++ src/components/IntroScreen.jsx
@@ -3,8 +3,14 @@ export default function IntroScreen({ onNext }) {
     <div className="screen intro">
       <h1>Velkommen til Idéernes Verden!</h1>
       <img src="/peggy.png" alt="Peggy" className="peggy-img" />
-      <p>Peggy venter på at tage dig med på en spændende rejse</p>
-      <button onClick={onNext} className="arrow">⬇️</button>
+      <p>
+      Peggy venter på at tage dig med på en spændende rejse <br/> <br/>
+      Tyk på knappen for at fortsætte
+      </p>
+      <button onClick={onNext} className="knap">
+        <img src="src\assets\knap.png" alt="knap-næste" />
+      </button>
+      <div className="black-bottom-shape"></div>
     </div>
   );
 }
--- src/components/PeggyIntro.jsx
+++ src/components/PeggyIntro.jsx
@@ -5,12 +5,15 @@ export default function PeggyIntro({ onNext }) {
       <img src="/peggy.png" alt="Peggy" className="peggy-img" />
       <p>
         Hej! Jeg er Peggy – og jeg kommer fra idéverdenen!
-        <br />
+        <br/> <br/>
         Jeg er her for at hjælpe dig med at tænke som en filosof.
-        <br />
+        <br/> <br/>
         Klar til at gå på opdagelse i en verden fuld af skjulte idéer?
       </p>
-      <button onClick={onNext} className="arrow">⬇️</button>
+      <button onClick={onNext} className="knap">
+        <img src="src\assets\knap.png" alt="knap-næste" />
+      </button>
+      <div className="black-bottom-shape"></div>
     </div>
   );
 }
